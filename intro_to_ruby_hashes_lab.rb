67824ea4c67f215e2a6705d81f1710fe3a7cc836
Viachaslau Pashaliuk <v.pashaliuk@gmail.com>
2019-09-10 03:12:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,7 +36,7 @@ def monopoly_with_third_tier
 	      :reading_railroad => {},
 	      :pennsylvania_railroad => {},
 	      :o_and_b_railroad => {},
-	      :jacks => {}}
+	      :jacks => {}
 	    }
 	  }
 	}
