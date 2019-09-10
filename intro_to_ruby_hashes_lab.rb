b43e47d79a792480cf156e064f936b9637bdd15b
Viachaslau Pashaliuk <v.pashaliuk@gmail.com>
2019-09-10 03:09:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ def monopoly_with_third_tier
 	  :railroads =>{
 	    :pieces => 4,
 	    :rent_in_dollars => {
-	      :dsd => 3,
+	      : => 3,
 	      :adsd => 4,
 	      :dsf => 5,
 	      :s => 6
